47eeafe32265d987757259e5258574f3d614e11c
liangjunren <liangjunren@bytedance.com>
2019-03-25 17:28:07 +0800

    FIX: removeListener called on Observable, should be on handler of Observable

--- test/observe.test.js
+++ test/observe.test.js
@@ -1,16 +1,13 @@
 /* eslint-env mocha */
 
-const assert = require('assert')
 const it = require('mocha-param')
 const observe = require('../lib/observe')
 describe('Observable', () => {
-  
   it('简单对象操作', [{}]
-    , (obj) => {
+    , () => {
       let x = observe({ a: { b: 1 } }, (change) => {
         console.log(change)
       })
       x.a.b = 2
     })
-  
 })
